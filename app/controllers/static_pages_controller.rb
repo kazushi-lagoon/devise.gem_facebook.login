86ea3ebb4844fbf5f690099b61e10fc7134cb2bb
EC2 Default User <ec2-user@ip-172-31-17-5.us-east-2.compute.internal>
2019-12-03 16:44:43 +0000

    Finish ch03

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    # app/views/リソース名/アクション名.html.erb
+    # app/views/static_pages/home.html.erb
+    
   end
 
   def help
+  end
+  
+  def about
   end
 end
