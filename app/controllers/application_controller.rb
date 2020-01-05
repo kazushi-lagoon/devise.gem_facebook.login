4830cc302575b3cd3942c9b95f979a13b71fe1e4
yoshida <kazu0226_7382@yahoo.co.jp>
2020-01-05 20:50:36 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include ActiveModel
   # helper　メソッドの元々の使い方は、。gravatar_forやfull_titleがそうだったように、viewで使うもの。デフォルトでview で使える。
   # controller　で使う場合は、include で読み込んで、使えるスコープを広げる必要がある。
   
